91270b9e0587c860475e58f3e9fca38f8c56afb8
Ahmad Khan <ahmadkhanuammya@gmail.com>
2024-01-21 22:21:34 +0500

    Different Java Patterns.

--- Prob1.java
+++ Prob1.java
@@ -1,6 +1,6 @@
 public class Prob1 {
     public static void main(String[] args) {
-        int rows = 5;
+        int rows = 4;
 //        for (int i=1; i<=rows; i++){
 //            for (int j=1; j<=rows-i; j++){
 //                System.out.print(" ");
@@ -14,13 +14,35 @@ public class Prob1 {
 //            }
 //            System.out.println();
 //        }
-        for (int i=1; i<=rows; i++){
-            for (int j=1; j<=rows-i; j++){
+//        for (int i=0; i<=rows; i++){
+//            int counter = 1;
+//            for (int j=0; j<=rows-i; j++){
+//                System.out.print(" ");
+//            }
+//            for (int k=0; k<=i; k++){
+//                System.out.print(" "+counter);
+//                counter = counter*(i-k)/(k+1);
+//            }
+//            System.out.println();
+//        }
+//        for (int i=1; i<=rows; i++){
+//            for (int j=1; j<=rows-i; j++){
+//                System.out.print(" ");
+//            }
+//            for (int k=1; k<=i; k++){
+//                System.out.print(k+" ");
+//            }
+//            System.out.println();
+//        }
+        int counter = 1;
+        for (int i=rows; i>=1; i--){
+            for (int j=1; j<=rows-i;j++){
                 System.out.print(" ");
             }
             for (int k=1; k<=i; k++){
-                System.out.print(i+" ");
+                System.out.print(" "+counter);
             }
+            counter++;
             System.out.println();
         }
     }
